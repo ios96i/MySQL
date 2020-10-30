0264d0eea6431d759386225e11ab0c656a501616
Osama Ismail Fatani <62897025+ios96i@users.noreply.github.com>
2020-10-30 17:19:08 +0300

    Update 2.AddDataOnTable.sql

--- 2.AddDataOnTable.sql
+++ 2.AddDataOnTable.sql
@@ -15,3 +15,16 @@ dep)values
 );
 
 select * from employee;
+
+
+##result 
++-------+-----------+----------+--------+------------+
+| empid | FirstName | LastName | salary | dep        |
++-------+-----------+----------+--------+------------+
+|     1 | Osama     | Ismail   | 300.50 | se         |
+|     2 | Waeel     | alrubaye |  90.00 | se         |
+|     3 | rafedd    | alrubaye | 900.00 | math       |
+|     4 | jena      | ahmed    | 200.00 | electronic |
+|     5 | ahmed     | salem    | 300.00 | math       |
++-------+-----------+----------+--------+------------+
+5 rows in set (0.00 sec)
